d8450e3889f7d3c6d810c54edacdf5a03ce323e8
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2016-04-25 20:57:38 -0300

    Configurado o acesso de qualquer origem(cross-origin) no server

--- src/main/java/br/com/todo/list/app/TodoListAppApplication.java
+++ src/main/java/br/com/todo/list/app/TodoListAppApplication.java
@@ -2,10 +2,24 @@ package br.com.todo.list.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class TodoListAppApplication {
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting-javaconfig").allowedOrigins("'https://pcfmello-todo-list-app.herokuapp.com");
+            }
+        };
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListAppApplication.class, args);
 	}
